915755df4bd610fbb7bd500e49d6f21b8c2f1de6
migaria <miarias1417@gmail.com>
2025-10-21 20:03:25 -0500

    Guardando cambios locales antes de rebase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { PerfilComponent } from './perfil/perfil.component';
 import { PerfilAgricultorComponent } from './perfil-agricultor/perfil-agricultor.component';
 import { PerfilCompradorComponent } from './perfil-comprador/perfil-comprador.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrarComponent } from './registrar/registrar.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'perfil', pathMatch: 'full' },
+  { path: '', component: LoginComponent, pathMatch: 'full' }, // 👈 login por defecto
   { path: 'perfil', component: PerfilComponent },
   { path: 'agricultor', component: PerfilAgricultorComponent },
-  { path: 'comprador', component: PerfilCompradorComponent }
-
+  { path: 'comprador', component: PerfilCompradorComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registrar', component: RegistrarComponent }
 ];
-
