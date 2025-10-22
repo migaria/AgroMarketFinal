0b13c371fcbdf2af8173942290ca9c43deb621f9
yonatan anacona <yonatananacona6@gmail.com>
2025-10-21 21:43:09 -0500

    Configuración inicial del proyecto

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { PerfilComponent } from './perfil/perfil.component';
+import { PerfilAgricultorComponent } from './perfil-agricultor/perfil-agricultor.component';
+import { PerfilCompradorComponent } from './perfil-comprador/perfil-comprador.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'perfil', pathMatch: 'full' },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'agricultor', component: PerfilAgricultorComponent },
+  { path: 'comprador', component: PerfilCompradorComponent }
+
+];
+
