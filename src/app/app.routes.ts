c08c79fe447f5937b9fca48c858692d5eb3d0064
dmuelas-blip <dmuelas@unimayor.edu.co>
2025-10-23 16:51:06 -0500

    Guardando mis cambios locales antes de actualizar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,15 @@ import { Routes } from '@angular/router';
 import { PerfilComponent } from './perfil/perfil.component';
 import { PerfilAgricultorComponent } from './perfil-agricultor/perfil-agricultor.component';
 import { PerfilCompradorComponent } from './perfil-comprador/perfil-comprador.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
+  
   { path: '', redirectTo: 'perfil', pathMatch: 'full' },
   { path: 'perfil', component: PerfilComponent },
   { path: 'agricultor', component: PerfilAgricultorComponent },
-  { path: 'comprador', component: PerfilCompradorComponent }
+  { path: 'comprador', component: PerfilCompradorComponent },
+  {path: 'login', component:LoginComponent}
 
 ];
 
