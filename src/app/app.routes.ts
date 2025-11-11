4fde9387cf33e807c34c2f696e9e27cdc85a0f8d
dmuelas-blip <dmuelas@unimayor.edu.co>
2025-11-11 14:37:22 -0500

    ajustes en botones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { PerfilCompradorComponent } from './perfil-comprador/perfil-comprador.co
 import { LoginComponent } from './login/login.component';
 import { RegistrarComponent } from './registrar/registrar.component';
 import { CarritoComprasComponent } from './carrito-compras/carrito-compras.component';
-
+import { InicioComponent } from './inicio/inicio.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full' }, // 👈 login por defecto
